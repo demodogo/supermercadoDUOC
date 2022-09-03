9a5cf69a23ca3e248117f4533284113245bb4272
demodogo <macaigm@gmail.com>
2022-09-02 23:24:57 -0400

    Supermercado DUOC

--- src/proyecto/supermercado/Cliente.java
+++ src/proyecto/supermercado/Cliente.java
@@ -1,5 +1,8 @@
 package proyecto.supermercado;
+import java.lang.StringBuilder;
 import java.util.Scanner;
+import static proyecto.supermercado.Producto.products;
+
 public class Cliente {
     static String rut;
     String nombre;
@@ -17,8 +20,7 @@ public class Cliente {
 
     public static void setCliente() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese su rut (con puntos y guión): ");
-        String rut = scanner.next();
+
         System.out.println("Nombre:");
         String nombre = scanner.next();
         System.out.println("Correo electrónico:");
@@ -27,34 +29,75 @@ public class Cliente {
         String direccion = scanner.next();
         System.out.println("Número de telefono (+56 9): ");
         int telefono = scanner.nextInt();
+        System.out.println("Ingrese su rut (sin puntos ni guión): ");
+        String rut = scanner.next();
 
         new Cliente(rut, nombre, mail, direccion, telefono);
-        // Cliente.validarRut(rut); commented because tira el medio error, compare.
+        Cliente.validarRut(rut);
     }
 
-    /*public static void validarRut(String rut) {
-        boolean esValido = false;
-        rut = rut.toUpperCase();
-        rut = rut.replace("-","");
-        rut = rut.replace("-","");
-        int rutAux = Integer.parseInt(rut.substring(0, rut.length() -1));
+    private static void validarRut(String rut) {
+        Cliente.rut = Cliente.rut.toUpperCase();
+        StringBuilder noDVrut = new StringBuilder(Cliente.rut.substring(0, rut.length() -1 ));
+        noDVrut.reverse();
+        String rutToString = noDVrut.toString();
+        char[] stringArray = rutToString.toCharArray();
+        int size = stringArray.length;
+        int [] numbers = new int[size];
 
-        char dv = rut.charAt(rut.length() - 1);
-        int m = 0, s = 1;
-        for (; rutAux != 0; rutAux /= 10) {
-            s = (s + rutAux % 10 * (9 - m++ % 6)) % 11;
+        for (int i = 0; i < size; i++) {
+            String toString = String.valueOf(Character.getNumericValue(stringArray[i]));
+            int toInt = Integer.parseInt(toString);
+            numbers[i] = toInt;
         }
-        if (dv == (char) (s != 0 ? s + 47 : 75)) {
-            esValido = true;
+        int multiplicador = 2;
+        int sumaDigitos = 0;
+        int result;
+
+        for (int number : numbers) {
+
+            if (multiplicador > 7) {
+                multiplicador = 2;
+            }
+            result = number * multiplicador;
+            sumaDigitos += result;
+
+
+            multiplicador++;
+        }
+        int resultDivision = sumaDigitos / 11;
+        int floorDivision = (int)Math.floor(resultDivision);
+        int resultMultiplicacion = floorDivision * 11;
+        int finalResult = sumaDigitos - resultMultiplicacion;
+        finalResult = finalResult - 11;
+        finalResult = Math.abs(finalResult);
+        System.out.println(finalResult);
+        if (finalResult == Integer.parseInt(rut.substring(rut.length() -1) ) || finalResult == 11 && Integer.parseInt(rut.substring(0, rut.length() -1)) == 0 || finalResult == 10 && (rut.substring(0, rut.length() -1)).equals("K")) {
+            System.out.println("Rut ingresado correctamente.");
+            Cliente.terminateQuestion();
+        } else {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Rut incorrecto o no existente.");
+            System.out.println("Ingrese su rut (sin puntos ni guión): ");
+            rut = scanner.next();
+            Cliente.rut = rut;
+            validarRut(Cliente.rut);
+        }
         }
 
-        if (!esValido) {
-            System.out.println("Boleta emitida. \n Gracias por comprar con nosotros");
+    private static void terminateQuestion() {
+        System.out.println("¿Desea volver al menú principal o cerrar el programa? \n OPCION (1) = CERRAR PROGRAMA \n OPCION (2) = VOLVER AL MENÚ. ");
+        Scanner scanner = new Scanner(System.in);
+        int opcion = scanner.nextInt();
+        if (opcion == 1) {
+            System.out.println("Gracias por comprar en nuestra tienda.");
+            System.exit(0);
+        } else if (opcion == 2) {
+            Menu.getMenu(products);
+        } else {
+            System.out.println("Opción inválida.");
+            terminateQuestion();
         }
-        else if (esValido) {
-            System.out.println("RUT INVALIDO, INGRESE SU RUT NUEVAMENTE.");
-            Cliente.setCliente();
-        }
-
-    }*/
+    }
 }
+
